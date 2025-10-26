012ffb13163e5fbbc5772bf8186487fa3040815c
punaram14 <prp8334@gmail.com>
2025-10-26 13:00:47 -0500

    web app

---
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -0,0 +1,17 @@
+package com.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebappApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebappApplication.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(WebappApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/webapp/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello from My Simple Spring Boot Web App!";
+    }
+}
